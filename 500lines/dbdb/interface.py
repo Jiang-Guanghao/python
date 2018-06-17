8fa058f0dde97e34d8d30d32b529f577096ca16a
Jiang Guanghao <jiangguanghao@hotmail.com>
2018-06-17 18:51:00 +0800

    eat dinner

---
+++ 500lines/dbdb/interface.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class DBDB(object):
+    def __init__(self, f):
+        
--- test.py
+++ test.py
@@ -1,5 +1,29 @@
 # /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+def maxof2(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+print(maxof2(10,100))
+
+x = 7 ** 6
+print (x)
+
+for i in range(1,10,1):
+    print(i)
+
+a = 0
+while a < 11:
+    a = a + 1
+    print(a)
+
+print("--------test open file--------")
+with open(".gitignore") as file:
+    for line in file:
+        print(line)
+
 if __name__ == "__main__":
     print("hello.")
