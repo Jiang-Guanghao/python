e9ac7c42e686d17f546b7a25edab116bd2ae1126
Jiang Guanghao <jiangguanghao@hotmail.com>
2018-06-25 17:59:56 +0800

    type code finish

--- 500lines/dbdb/logical.py
+++ 500lines/dbdb/logical.py
@@ -32,6 +32,22 @@ class ValueRef(object):
             self._address = storage.write(self.referent_to_string(self._referent))
 
 class LogicalBase(object):
+    node_ref_class = None
+    value_ref_class = ValueRef
+
+    def __init__(self, storage):
+        self._storage = storage
+        self._refresh_tree_ref()
+    
+    def commit(self):
+        self._tree_ref.store(self._storage)
+        self._storage.commit_root_address(self._tree_ref.address)
+
+    def _refresh_tree_ref(self):
+        self._tree_ref = self.node_ref_class(
+            address = self._storage.get_root_address()
+            )
+    
     def get(self, key):
         if not self._storage.locked:
             self._refresh_tree_ref()
@@ -40,18 +56,27 @@ class LogicalBase(object):
     def set(self, key, value):
         if self._storage.locked:
             self._refresh_tree_ref()
-        self._tree_ref = self._insert(self._follow(self._tree_ref), key, self.value_ref_class(value))
-        
-    def commit(self):
-        self._tree_ref.store(self._storage)
-        self._storage.commit_root_address(self._tree_ref.address)
+        self._tree_ref = self._insert(
+            self._follow(self._tree_ref), key, self.value_ref_class(value)
+            )
 
-    def store(self, storage):
-        if self._referent is not None and not self._address:
-            self.prepare_to_store(storage)
-            self._address = store.write(self.referent_to_string(self._regerent))
+    def pop(self, key):
+        if self._storage.lock():
+            self._refresh_tree_ref()
+        self._tree_ref = self._delete(
+            self._follow(self._tree_ref), key
+            )
+    
+    def _follow(self, ref):
+        return ref.get(self._storage)
 
-    def _refresh_tree_ref(self):
-        self._tree_ref = self.node_ref_class(address=self._storage._get_root_assress())
+    def __len__(self):
+        if not self._storage.locked:
+            self._refresh_tree_ref()
+        root = self._follow(self._tree_ref)
+        if root:
+            return root.length
+        else:
+            return 0
 
     
